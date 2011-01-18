3cd8175d80821098c3bf6fa99de92a2ccd4ecc04
Ali Moeeny <alimoeeny@gmail.com>
2011-01-18 08:28:12 -0500

    Clients can't request more than 150 times in an hour. That adds up to one every 30 seconds just to make it easy. And I need to sanitize at least the tweet text and urls. Remove tabs (I am using tabs to split the text for now) and line breakes.

--- pubIndexWriter.py
+++ pubIndexWriter.py
@@ -1,29 +1,36 @@
 import tweepy
 import time
 
+def sanitize(s):
+	r = s.replace("\t","");
+	r = s.replace("\n","");
+	r = s.replace("\r","");
+	return r
+
 if __name__ == "__main__":
+	splitter = "\t";
 	f = open("pubT.txt", 'w');
 	#for i in range(0,10):
 	while (True):
-		time.sleep(10);
+		time.sleep(30);
 		try:
 			pub_tw = tweepy.api.public_timeline();
 			for t in pub_tw:
 				s = '';
-				s += t.id.__str__() + '-';
-				s += t.user.id.__str__() + '-';
-				s += t.author.id.__str__() + '-';
-				s += t.created_at.__str__() + '-';
+				s += t.id.__str__() + splitter;
+				s += t.user.id.__str__() + splitter;
+				s += t.author.id.__str__() + splitter;
+				s += t.created_at.__str__() + splitter;
 				if t.source_url==None :
-					s += '-';
+					s += splitter;
 				else:				
-					s += t.source_url + '-';						
+					s += sanitize(t.source_url) + splitter;						
 				if t.place==None :
-					s += '-';
+					s += splitter;
 				else:				
-					s += t.place['country'] + '-';						
-				#s += t. + '-';						
-				s += t.text;
+					s += t.place['country'] + splitter;						
+				#s += t. + splitter;						
+				s += sanitize(t.text) + splitter;
 				f.write(s.encode('utf-8'));
 				f.write("\n");	
 		finally:
